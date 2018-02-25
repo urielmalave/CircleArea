82471727f8ab02b1638415ee1783c3de5722ebcb
urielmalave <urielmalave@upr.edu>
2018-02-25 12:02:29 -0400

    Area calculation

--- CircleArea.java
+++ CircleArea.java
@@ -3,15 +3,14 @@ import java.util.Scanner;
 public class CircleArea {
 
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
-
+		
 		System.out.print("Enter radius of circle: ");
-		int r=sc.nextInt();
+		int r = sc.nextInt();
 		double area = Math.PI*r*r;
-		double halfArea = area/2;
-
-		System.out.println(halfArea);
-
+	
+		System.out.println("The area of the cirle is " +area);
 	}
-}		
+}
+

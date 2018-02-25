c919e6ebb052d066b1340ff9360ae709c2f1057a
urielmalave <urielmalave@upr.edu>
2018-02-25 12:04:36 -0400

    Added permiter feature

--- CircleArea.java
+++ CircleArea.java
@@ -9,8 +9,9 @@ public class CircleArea {
 		System.out.print("Enter radius of circle: ");
 		int r = sc.nextInt();
 		double area = Math.PI*r*r;
-	
-		System.out.println("The area of the cirle is " +area);
+		double halfArea = area/2;
+
+		System.out.println("The halfArea of the cirle is " +halfArea);
 	}
 }
 
